be550604278b13fc03dd16ea4669ba3df8d624f9
zytron14 <grant.c.prater@gmail.com>
2020-06-02 11:20:37 -0500

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,16 @@
+class TagsController < ApplicationController
+    def index
+        @tags = Tag.all
+    end
+
+    def show
+        @tag = Tag.find(params[:id])
+    end
+
+    def destroy
+        @tag = Tag.find(params[:id]).destroy
+        flash.notice = "Tag '#{@tag.name}' deleted!"
+        redirect_to articles_path
+    end
+
+end
